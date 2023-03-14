9a1dcf010f96762d5e1afb51b4f332cbd973e5a3
eduardamendonca <eduardamendonca.dev@gmail.com>
2023-03-14 09:58:48 -0300

    0.0.1

---
+++ php/aprendendo.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toda a documentação de php no curso em video</title>
+</head>
+<body>
+    <div>
+        <h1>Variáveis em PHP</h1>
+        <?php
+        $idade = 18;
+        $nome = "Eduarda";
+        echo $nome." tem ".$idade." anos!";
+        echo "$nome tem $idade anos!";
+        ?>
+    </div>
+    <div>
+        <h1>Operadores aritméticos</h1>
+        <?php
+        
+        ?>
+    </div>
+</body>
+</html>
+
+
