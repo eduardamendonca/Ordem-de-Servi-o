d08f933860b6ed89b47aa88a688b99ec2195f2c7
eduardamendonca <eduardamendonca.dev@gmail.com>
2023-03-16 00:48:01 -0300

    0.0.1

--- php/infcliente.php
+++ php/infcliente.php
@@ -7,17 +7,18 @@ $db = mysql_select_db('os');
         $query = "INSERT INTO inf_cliente (telefone1) VALUES ('$telefone')";
         $insert = mysql_query($query,$connect);*/
         
-$nome = $_POST["nome"];
+$nome = isset ($_POST["nome"])?$_POST["nome"]:null;
 $telefone1 = $_POST["telefone1"];
-$telefone2 = $_POST["telefone2"];
-$cep = $_POST["cep"];
+$telefone2 = isset ($_POST["telefone2"])?$_POST["telefone2"]:null;
+$cep = isset ($_POST["cep"])?$_POST["cep"]:null;
 $cidade = $_POST["cidade"];
-$rua = $_POST["rua"];
-$num = $_POST["num"];
-$bairro = $_POST["bairro"];
+$rua = isset ($_POST["rua"])?$_POST["rua"]:null;
+$num = isset ($_POST["num"])?$_POST["num"]:null;
+$bairro = isset ($_POST["bairro"])?$_POST["bairro"]:null;
 
 echo "Seu nome é $nome";
 echo "<br/>Você possui o telefone $telefone1";
+echo "<br/>Você possui o segundo telefone $telefone2";
 
-        $valor=$_POST["nome"];
+
 ?>
