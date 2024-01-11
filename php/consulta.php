335e692c18efbead17f2dc3ce8a8b96496d07b91
Eduarda Mendonça <eduardamendonca.dev@gmail.com>
2024-01-11 09:47:03 -0300

    configurando pdf.php e consulta.html

--- php/consulta.php
+++ php/consulta.php
@@ -1,5 +1,37 @@
 <?php
+    $modo = isset ($_POST['modo']);
+    $busca = isset ($_POST['busca']);
+    echo $modo;
+
+    if($modo !== "none"){
+        if($modo == "placa"){
+            echo "O modo é placa";
+        }
+        if($modo == "veiculo"){
+            echo "O modo é veiculo";
+        }
+        if($modo == "codigo"){
+            echo "O modo é codigo";
+        }
+        if($modo == "nome"){
+            echo "O modo é nome";
+        }
+        if($modo == "data"){
+            echo "O modo é data";
+        }
+
+    }else{
+        echo("<script> 
+                alert('Selecione o modo de consulta!'); 
+                window.location.href= '/automecanicapj/Ordem-de-Servico/consulta.html';
+            </script>");
+    }
+
+
+
+    /*
+    
     $sql= "SELECT * FROM $table as pedidos JOIN usuarios ON pedidos.mat_pedinte = usuarios.mat where id = '{$id}' ";
     $resultado = mysqli_query($conexao,$sql) or die();
-    $row = mysqli_num_rows($resultado);
+    $row = mysqli_num_rows($resultado);*/
 ?>
--- php/pdf.php
+++ php/pdf.php
@@ -121,92 +121,105 @@ include("conection.php");
 
     ?>
 <body>
-    <header>
-        <img src="/automecanicapj/Ordem-de-Servico/img/cabecalho.svg" >
-    </header>
-    <div class="inf">
-        <div class="info_cliente">
-            <p>Nome: <?php echo $nome;?></p> 
-        </div>
-        <div class="info_cliente">
-            <p>Endereço: <?php echo "$rua, $num, $bairro";?></p>
-            <p>Telefone: <?php echo $cliente;?> </p>
-        </div>
-        <div class="info_cliente">
-            <p>Km: <?php echo $km;?></p>
-            <p>Ano: <?php echo $ano;?></p>
-            <p>Combustível: <?php echo $combustivel;?></p>
-            <p>Motor: <?php echo $motor;?></p>
-        </div>
-        <div class="info_cliente">
-            <p>Veículo: <?php echo $carro;?></p>
-            <p>Placa <?php echo $veiculo;?></p>
-            <p>Cor: <?php echo $cor;?></p>
-        </div>
-    </div>
-    <div >
-        
-        <table border="1" class="table">
-            <thead class="thead">
-                <tr>
-                    <th class="qnt">Qnt.</th>
-                    <th class="descricao">Descrição</th>
-                    <th class="unit">P. Unit.</th>
-                    <th class="result">Total</th>
-                </tr> 
-            </thead> 
-            <?php
-            /* Consulta na tabela tabela_auxiliar -> servicos */
-                $servicos_querry = "SELECT quantidade, descricao, unidade, total
-                    FROM servicos WHERE 
-                    id = '$servico1' OR id = '$servico2' OR id = '$servico3' OR id = '$servico4' OR id = '$servico5' OR id = '$servico6' OR 
-                    id = '$servico7' OR id = '$servico8' OR id = '$servico9' OR id = '$servico10' OR id = '$servico11' OR id = '$servico12' OR
-                    id = '$servico13' OR id = '$servico14' OR id = '$servico15'
-                    ORDER BY id ASC";
+    <div class="ordem_servico">
 
-                $result_servicos = $mysqli->query($servicos_querry);
-
-                $resultado_total="0";
-                while ($servicos_data = mysqli_fetch_assoc($result_servicos) ){
-                    echo"<tr>";
-                    echo "<td>".$servicos_data['quantidade']."</td>";
-                    echo "<td>".$servicos_data['descricao']."</td>";
-                    echo "<td>".$servicos_data['unidade']."</td>";
-                    echo "<td>".$servicos_data['total']."</td>";
-                    echo"</tr>";
-                    
-                    $resultado_total += $servicos_data['total'];
-
-                    $num_rows = mysqli_num_rows($result_servicos);
-                    $teste = " ";
-                    if ($num_rows < 20) {
-                        for ($i = 0; $i < (20 - $num_rows); $i++) {
-                            echo "<tr>";
-                            echo "<td >.$teste</td>";
-                            echo "<td ></td>";
-                            echo "<td ></td>";
-                            echo "<td ></td>";
-                            echo "</tr>";
+        <header>
+            <img src="/automecanicapj/Ordem-de-Servico/img/cabecalho.svg" >
+        </header>
+        <div class="inf">
+            <div class="info_cliente">
+                <p>Nome: <?php echo $nome;?></p> 
+            </div>
+            <div class="info_cliente">
+                <p>Endereço: <?php echo "$rua, $num, $bairro";?></p>
+                <p>Telefone: <?php echo $cliente;?> </p>
+            </div>
+            <div class="info_cliente">
+                <p>Km: <?php echo $km;?></p>
+                <p>Ano: <?php echo $ano;?></p>
+                <p>Combustível: <?php echo $combustivel;?></p>
+                <p>Motor: <?php echo $motor;?></p>
+            </div>
+            <div class="info_cliente">
+                <p>Veículo: <?php echo $carro;?></p>
+                <p>Placa <?php echo $veiculo;?></p>
+                <p>Cor: <?php echo $cor;?></p>
+            </div>
+        </div>
+        <div >
+            
+            <table border="1" class="table">
+                <thead class="thead">
+                    <tr>
+                        <th class="qnt">Qnt.</th>
+                        <th class="descricao">Descrição</th>
+                        <th class="unit">P. Unit.</th>
+                        <th class="result">Total</th>
+                    </tr> 
+                </thead> 
+                <?php
+                /* Consulta na tabela tabela_auxiliar -> servicos */
+                    $servicos_querry = "SELECT quantidade, descricao, unidade, total
+                        FROM servicos WHERE 
+                        id = '$servico1' OR id = '$servico2' OR id = '$servico3' OR id = '$servico4' OR id = '$servico5' OR id = '$servico6' OR 
+                        id = '$servico7' OR id = '$servico8' OR id = '$servico9' OR id = '$servico10' OR id = '$servico11' OR id = '$servico12' OR
+                        id = '$servico13' OR id = '$servico14' OR id = '$servico15'
+                        ORDER BY id ASC";
+    
+                    $result_servicos = $mysqli->query($servicos_querry);
+    
+                    $resultado_total="0";
+                    while ($servicos_data = mysqli_fetch_assoc($result_servicos) ){
+                        echo"<tr>";
+                        echo "<td>".$servicos_data['quantidade']."</td>";
+                        echo "<td>".$servicos_data['descricao']."</td>";
+                        echo "<td>".$servicos_data['unidade']."</td>";
+                        echo "<td>".$servicos_data['total']."</td>";
+                        echo"</tr>";
+                        
+                        $resultado_total += $servicos_data['total'];
+    
+                        $num_rows = mysqli_num_rows($result_servicos);
+                        $teste = " ";
+                        if ($num_rows < 20) {
+                            for ($i = 0; $i < (20 - $num_rows); $i++) {
+                                echo "<tr>";
+                                echo "<td >.$teste</td>";
+                                echo "<td ></td>";
+                                echo "<td ></td>";
+                                echo "<td ></td>";
+                                echo "</tr>";
+                            }
                         }
                     }
-
-                    
-                }
-                    
-            ?>   
-            
-            
-        </table>
+                        
+                ?>   
+                
+                
+            </table>
+        </div>
+        <div class="info_cliente">
+            <p>Obs: <?php echo $obs;?></p>
+            Total R$ <?php echo $resultado_total;?>
+        </div>
+        <div class="info_cliente">
+            <p>90 dias de garantia do serviço</p>
+            <p>Data:<?php echo $hoje;?> </p>
+            <p>Cód:<?php echo $id_ordem_servico;?></p>
+        </div>
     </div>
-    <div class="info_cliente">
-        <p>Obs: <?php echo $obs;?></p>
-        Total R$ <?php echo $resultado_total;?>
-    </div>
-    <div class="info_cliente">
-        <p>90 dias de garantia do serviço</p>
-        <p>Data:<?php echo $hoje;?> </p>
-        <p>Cód:<?php echo $id_ordem_servico;?></p>
-    </div>
+    <section class="btn">
+        <a href="/automecanicapj/Ordem-de-Servico/" target="_self">
+            <button class="advance-btn">
+                <img src="/automecanicapj/Ordem-de-Servico/img/left.svg" class="img-btn">
+            </button> 
+        </a>
+        <a href="#" target="_self">
+            <button onclick="window.print()" class="imprimir-btn" type="submit">
+                <img src="/automecanicapj/Ordem-de-Servico/img/imprimir.svg" class="img-btn">
+            </button> 
+        </a>
+    </section>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap');
     *{
@@ -218,8 +231,10 @@ include("conection.php");
     body{
         margin: 15rem;
         margin-top: 2rem;
+        margin-bottom: 2rem;
         font-weight: 600;
         font-size: 1.5rem;
+        background-color: #00255C; 
     }
     img{
         width: 100%;
@@ -265,9 +280,51 @@ include("conection.php");
     input{
         margin: 0rem 0;
         padding: 0.1rem 0.1rem;
-
         width: 100%;
     }
+    .ordem_servico{
+        background-color: #ffffff;
+        margin: auto;
+        border-radius: 20px;
+        padding: 3rem;
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+    }
+    .btn{
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: row;
+        justify-content: space-around;
+        gap: 30rem;
+        padding: 2%;
+        margin-top: 6.37%;
+    }
+    .advance-btn,
+    .imprimir-btn{
+        background-color: #ffffff;
+        width: 10rem;
+        height: 5rem;
+        border-style:solid;
+        border-color: #00255C;
+        border-radius: 1.5rem;
+        cursor: pointer;   
+    }
+    .img-btn{
+        width: 4rem;
+    }
+    @media print{
+    body * {
+        visibility: hidden;
+      }
+      .ordem_servico * {
+        visibility: visible;
+      }
+      .ordem_servico {
+        position: fixed;
+        left: 10px;
+        top: 0px;
+      }
+}
     </style>
 </body>
 </html>
